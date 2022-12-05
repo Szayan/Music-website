64a811958f3542f66e526901de9c6c996ea97831
Shayan <shayan.bayat@elev.ga.lbs.se>
2022-12-05 16:17:01 +0100

    script update

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const playButton = document.querySelector('.play');
 const pauseButton = document.querySelector('.pause');
 var audio = document.getElementById("audio");
+var clicks = 0;
  
 function toggle() {
 audio.play();
+
+};
+
+function isEven(clicks) {
+	if (n%2 == 0)
+		return true;
+	else
+        return false;
+        audio.pause(false);
 }
+
+
