8baa3aa0ca3c5158e2c28f03a4f1b350f771f5ad
JasperLBS <77491565+JasperLBS@users.noreply.github.com>
2023-01-23 13:52:51 +0100

    PROGRESS BAR

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const playButton = document.querySelector('.play');
 const pauseButton = document.querySelector('.pause');
 const myAudio = document.getElementById("audio");
 
+
+	
+
 function toggle() {
 	if (!myAudio.paused && !myAudio.ended) {
 		myAudio.pause();
@@ -11,16 +14,26 @@ function toggle() {
 	}
 }
 
-<<<<<<< Updated upstream
+function move() {
+	var elem = document.getElementById("myBar");   
+	var width = 1;
+	var id = setInterval(frame, 10);
+	function frame() {
+	  if (width >= 100) {
+		clearInterval(id);
+	  } else {
+		width++; 
+		elem.style.width = width + '%'; 
+	  }
+	}
+  }
+
 document.getElementById('buttonid').addEventListener('click', uploadFile);
-function uploadFile()
-{
+function uploadFile(){
 document.getElementById('fileid').click()
 }
 
-=======
 function upload() {
 
 }
->>>>>>> Stashed changes
 
