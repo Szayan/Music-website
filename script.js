f628e20c10d730835afcd5a9ee368a9a6657c7e4
JasperLBS <77491565+JasperLBS@users.noreply.github.com>
2023-01-30 13:12:01 +0100

    cleanup

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const playButton = document.querySelector('.play');
 const pauseButton = document.querySelector('.pause');
 const myAudio = document.getElementById("audio");
 
-
 	
-
 function toggle() {
 	if (!myAudio.paused && !myAudio.ended) {
 		myAudio.pause();
